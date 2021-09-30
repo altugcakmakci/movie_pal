b145c60e3bee6f0b9255a9cb8b9dc3b403717d4c
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2021-09-30 14:55:32 -0400

    reddit api and add info

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ let modalSpan = $(".close");
 let searchBtnMovie =document.getElementById('movie-button');
 let searchMovie= document.getElementById('search-movie');
 
+let testStr="https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=shrek_2001"
 function searchForMovie(){
     // checks for valid search parameters
     if (!searchMovie.value || /^\s*$/.test(searchMovie.value)){
@@ -37,3 +38,38 @@ modalSpan.on("click", function(event) {
     alertModal.css("display", "none");
  })
 
+
+function additionalInfoMovie(){
+
+    //top reddit posts
+    //let queryString= movieTitle.trim().replace(" ","_");
+    let queryString='shrek'
+    fetch("https://www.reddit.com/r/" +queryString+"/top.json?count=20")
+    .then(function (response){
+        if (response.ok){
+            response.json().then(function (data){
+                console.log(data);
+               //only display text post discussions
+                for (i=0; i<data.data.children.length;i++){
+                   if (data.data.children[i].data.selftext==""){
+                       console.log("no text");
+                       //display nothing if there is no text in post
+                   }
+                   else{
+                    console.log("header"+data.data.children[i].data.title);
+                    console.log("text= "+data.data.children[i].data.selftext);
+                    // dynamically add elements data to the related media div
+                   }
+                
+                }
+            })
+        }
+        else{
+
+        }
+    });
+        
+ 
+}
+
+additionalInfoMovie();
