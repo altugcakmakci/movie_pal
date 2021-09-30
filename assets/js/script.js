e551490cc5f929f7feefd924d03e9915118a6c83
Philip Tantsidis <ptantsidis@gmail.com>
2021-09-30 10:10:43 -0400

    modal Working index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,19 @@
-let bookInput = $('#search-book').value;
-let authorInput = $('#search-author').value;
-let movieInput = $('#search-movie').value;
-let alertModal = document.getElementById("alert-modal");
-let modalSpan = document.getElementsByClassName("close")[0];
+let bookInput = $('#btnContainer');
+let searchInput = $("#stacked-search")
+let alertModal = $("#alert-modal");
+let modalSpan = $(".close");
 
 
-$(".book-btn").on("click",function(event){ 
+bookInput.on("click",function(event){ 
     event.preventDefault();
-    console.log(alertModal.childElementCount)
-    if (bookInput != ""); {
-        // alertModal.
-        // append("<p>").value = "Book Search Cannot be Empty!"
-        alertModal.style.display = "block";
+    console.log(searchInput.val());
+    if (searchInput.val() != ""); {
+        alertModal.css("display", "block");
     }   
-
 })
-$(".author-btn").on("click",function(event){ 
-    event.preventDefault();
-    if (authorInput != ""); {
-        alert(authorInput)
-        alertModal.append("Boo Error");
-    }   
 
-})
-$(".movie-btn").on("click",function(event){ 
+modalSpan.on("click", function(event) {
     event.preventDefault();
-    console.log("movie button click")
-    if (movieInput != ""); {
-        alert(movieInput)
-        alertModal.append("Boo Error");
-    }   
-
-})
-modalSpan.onclick = function(event) {
-    event.preventDefault();
-    alertModal.style.display = "none";
-}
-window.onclick = function(event) {
-    event.preventDefault();
-    if (event.target == alertModal) {
-      alertModal.style.display = "none";
-    }
-  }
+    console.log(modalSpan)
+    alertModal.css("display", "none");
+ })
